7ed492e7d4e5e1abf9dec515057e4fc89c8b2e2c
Dirk Kok <the@dirkkok.nl>
2022-06-12 13:42:09 +0200

    change version to 1.18 and fix mixin

--- src/main/java/dev/foxite/fabric/stoppanfromburningdowntheirhouse/mixin/FireBlockMixin.java
+++ src/main/java/dev/foxite/fabric/stoppanfromburningdowntheirhouse/mixin/FireBlockMixin.java
@@ -5,12 +5,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FireBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Mixin(FireBlock.class)
